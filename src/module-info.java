25cad9e830d59a9918c73a9fb27630bd0f9741b2
ambur <ambur@LAPTOP-BPJT3S4B>
2024-11-17 17:30:45 +0530

    firstCommitByAjay

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ambur
+ *
+ */
+module AJaySample {
+}
---
+++ src/samplepackage/Ajayy.java
@@ -0,0 +1,9 @@
+package samplepackage;
+
+public class Ajayy {
+	public static void main(String[] args) {
+		System.out.println(" ajay is great isnt it");
+		
+	}
+
+}
